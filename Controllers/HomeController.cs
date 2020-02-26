b565c4a7ea5ddf94bfb5420e47e4789f898aadf4
Daniil Shushakov <4shushakov@gmail.com>
2020-02-26 16:27:11 +0700

    remove garbage from old searchContorller and fix russian text

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,9 +12,13 @@ namespace Kenguru_four_.Controllers
         public ActionResult Index()
         {
             KenguruDB db = new KenguruDB();
+            db.goods.Add(new Good { title = "Носки без дырок", categoryID = 7, sellerID = -3, short_discribe = "", count = 2, description = "", price = 100, seles = 1 });
+            db.goods.Add(new Good { title = "Носки с кошками", categoryID = 7, sellerID = -3, short_discribe = "", count = 2, description = "", price = 700, seles = 1 });
+            db.goods.Add(new Good { title = "Носки с дырками и с кошками", categoryID = 7, sellerID = -3, short_discribe = "", count = 2, description = "", price = 350, seles = 1 });
+            db.goods.Add(new Good { title = "Носки как носки может с дырками", categoryID = 7, sellerID = -3, short_discribe = "", count = 2, description = "", price = 400, seles = 1 });
+            db.goods.Add(new Good { title = "Носки для богатых", categoryID = 7, sellerID = -3, short_discribe = "", count = 2, description = "", price = 3000, seles = 1 });
+            db.SaveChanges();
             return View(db);
         }
-
-     
     }
 }
