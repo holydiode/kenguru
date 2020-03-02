f908de5c008ab1296d8a1b3ca191f880b9f33948
Daniil Shushakov <4shushakov@gmail.com>
2020-03-02 22:28:10 +0700

    Если хочешь посмотреть, где заказ твой пропадает, то введи трек номер свой, сайт в бд все разыскает bonus: стрелочки в нумерации страниц товаров

---
+++ Models/ViewModels/PageInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kenguru_four_.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int PageNumber { get; set; } // номер текущей страницы
+        public int PageSize { get; set; } // кол-во объектов на странице
+        public int TotalItems { get; set; } // всего объектов
+        public int TotalPages   // всего страниц
+        {
+            get {
+                float res = (TotalItems) / PageSize;
+                if (TotalItems % PageSize != 0)
+                    res += 1;
+                return (int)res;
+            }
+        }
+
+    }
+}
